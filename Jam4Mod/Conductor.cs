16dc11bcffbee74aee86d7e5a39321af6d89606f
ditzy <ditzy.zy@pm.me>
2024-10-22 21:57:09 -0500

    add base EOLS scripts

---
+++ Jam4Mod/Conductor.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+namespace Jam4Mod;
+
+public class Conductor : MonoBehaviour
+{
+  [SerializeField] private EndOfLoopController endOfLoopController;
+}
---
+++ Jam4Mod/EndOfLoopController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UnityEngine;
+
+namespace Jam4Mod;
+
+public class EndOfLoopController : MonoBehaviour
+{
+  [SerializeField] private Transform spawnPoint;
+  [SerializeField] private OWTriggerVolume trigger;
+
+  private void Awake()
+  {
+    trigger.OnEntry += TriggerEntered;
+  }
+
+  private void OnDestroy()
+  {
+    trigger.OnEntry -= TriggerEntered;
+  }
+
+  private void TriggerEntered(GameObject hitObj)
+  {
+    Debug.LogError("triggered");
+    if (!hitObj.CompareTag("PlayerDetector")) return;
+
+    var player = Locator.GetPlayerBody();
+    player.SetPosition(spawnPoint.position);
+    player.SetRotation(spawnPoint.rotation);
+  }
+}

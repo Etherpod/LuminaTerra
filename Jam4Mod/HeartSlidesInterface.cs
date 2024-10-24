9435ac0c62acf51ad17213df070a7e8bcfd0f099
Etherpod <etherpod@gmail.com>
2024-10-23 23:15:00 -0700

    Add effects to heart vision

---
+++ Jam4Mod/HeartSlidesInterface.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+namespace Jam4Mod;
+
+public class HeartSlidesInterface : MonoBehaviour
+{
+    [SerializeField]
+    private OWTriggerVolume _heartRoomTrigger;
+
+    private MindProjectorTrigger _projector;
+    private bool _hasActivated = false;
+
+    private void Awake()
+    {
+        _projector = GetComponentInChildren<MindProjectorTrigger>();
+    }
+
+    private void Start()
+    {
+        _heartRoomTrigger.OnEntry += OnEntry;
+    }
+
+    private void OnEntry(GameObject hitObj)
+    {
+        if (!_hasActivated && hitObj.CompareTag("PlayerDetector"))
+        {
+            _projector.SetProjectorActive(true);
+            _hasActivated = true;
+        }
+    }
+
+    private void OnDestroy()
+    {
+        _heartRoomTrigger.OnEntry -= OnEntry;
+    }
+}

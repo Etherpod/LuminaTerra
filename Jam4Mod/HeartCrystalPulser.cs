a00d8853b4ac1ccd3c5c6d496760ec48b55b54a2
Etherpod <etherpod@gmail.com>
2024-10-23 22:17:30 -0700

    Added heart pulse

---
+++ Jam4Mod/HeartCrystalPulser.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using UnityEngine;
+
+namespace Jam4Mod;
+
+public class HeartCrystalPulser : MonoBehaviour
+{
+    [SerializeField]
+    private float _pulseSpeed;
+    [SerializeField]
+    private float _minEmission;
+    [SerializeField]
+    private float _maxEmission;
+
+    OWEmissiveRenderer _renderer;
+
+    private void Awake()
+    {
+        _renderer = GetComponent<OWEmissiveRenderer>();
+    }
+
+    private void Update()
+    {
+        float nextEmissive = Mathf.Lerp(_minEmission, _maxEmission, (Mathf.Sin(Time.time * _pulseSpeed) + 1) / 2);
+        _renderer.SetEmissiveScale(nextEmissive);
+    }
+}

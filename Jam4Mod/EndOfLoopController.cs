7014f4f761dbf196fcba053d89533071d217c946
Etherpod <etherpod@gmail.com>
2024-10-27 02:11:34 -0700

    Improve EOLS visuals

--- Jam4Mod/EndOfLoopController.cs
+++ Jam4Mod/EndOfLoopController.cs
@@ -4,41 +4,51 @@ namespace Jam4Mod;
 
 public class EndOfLoopController : MonoBehaviour
 {
-  private static readonly float SunScale = 0.8f;
+    private static readonly float SunScale = 0.3f;
 
-  [SerializeField] private Transform playerSpawn = null;
-  [SerializeField] private Transform sunSpawn = null;
-  [SerializeField] private MeshRenderer bounds = null;
-  [SerializeField] private ParticleSystem stars = null;
-  [SerializeField] private GameObject[] refs = null;
+    [SerializeField] private Transform playerSpawn = null;
+    [SerializeField] private Transform sunSpawn = null;
+    [SerializeField] private MeshRenderer bounds = null;
+    [SerializeField] private ParticleSystem stars = null;
+    [SerializeField] private GameObject[] refs = null;
 
-  public void Awake()
-  {
-    bounds.enabled = false;
-    foreach (var refObj in refs)
+    public void Awake()
     {
-      refObj.SetActive(false);
+        bounds.enabled = false;
+        foreach (var refObj in refs)
+        {
+            refObj.SetActive(false);
+        }
     }
-  }
 
-  public void StartEOLS()
-  {
-    gameObject.SetActive(true);
+    public void StartEOLS()
+    {
+        gameObject.SetActive(true);
 
-    stars.Play();
+        stars.Play();
 
-    var sun = Instantiate(
-      GameObject.Find("Jam4Sun_Body/Sector/Star/Surface"),
-      sunSpawn.position,
-      sunSpawn.rotation,
-      transform
-    );
-    sun.transform.localScale = new Vector3(SunScale, SunScale, SunScale);
+        var sun = Instantiate(
+          GameObject.Find("Jam4Sun_Body/Sector/Star/Surface"),
+          sunSpawn.position,
+          sunSpawn.rotation,
+          transform
+        );
+        sun.transform.localScale = Vector3.one * SunScale;
 
-    var player = Locator.GetPlayerBody();
-    player.SetPosition(playerSpawn.position);
-    player.SetRotation(playerSpawn.rotation);
+        var atmo = Instantiate(
+            GameObject.Find("Jam4Sun_Body/Sector/Star/Atmosphere_Star/AtmoSphere/Atmosphere_LOD2"),
+            sunSpawn.position,
+            sunSpawn.rotation,
+            transform);
+        atmo.transform.localScale = Vector3.one * SunScale * 1.2f;
 
-    Locator.GetPlayerSuit().RemoveSuit(true);
-  }
+        var player = Locator.GetPlayerBody();
+        player.SetPosition(playerSpawn.position);
+        player.SetRotation(playerSpawn.rotation);
+        player.SetVelocity(gameObject.GetAttachedOWRigidbody().GetVelocity());
+
+        Locator.GetPlayerSuit().RemoveSuit(true);
+        Locator.GetFlashlight().TurnOff();
+        Locator.GetToolModeSwapper().UnequipTool();
+    }
 }

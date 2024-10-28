b2f0909a7ecae7f28c0dc0c862bdb5323128f93e
Etherpod <etherpod@gmail.com>
2024-10-28 14:26:11 -0700

    Add ambient music trigger

---
+++ LuminaTerra/AmbientMusicTrigger.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+public class AmbientMusicTrigger : MonoBehaviour
+{
+    [SerializeField] private float _fadeTime;
+
+    private OWTriggerVolume _trigger;
+    private OWAudioSource _audio;
+
+    private void Awake()
+    {
+        _trigger = GetComponent<OWTriggerVolume>();
+        _audio = GetComponent<OWAudioSource>();
+    }
+
+    private void Start()
+    {
+        _trigger.OnEntry += OnEntry;
+        _trigger.OnExit += OnExit;
+    }
+
+    private void OnEntry(GameObject hitObj)
+    {
+        if (hitObj.CompareTag("PlayerDetector"))
+        {
+            _audio.FadeIn(_fadeTime);
+        }
+    }
+
+    private void OnExit(GameObject hitObj)
+    {
+        if (hitObj.CompareTag("PlayerDetector"))
+        {
+            _audio.FadeOut(_fadeTime);
+        }
+    }
+
+    private void OnDestroy()
+    {
+        _trigger.OnEntry -= OnEntry;
+        _trigger.OnExit -= OnExit;
+    }
+}

dee3754b74d1f94833c5502b5c71bc79de34f81d
Etherpod <etherpod@gmail.com>
2024-11-03 12:31:43 -0800

    Door anim

---
+++ LuminaTerra/MysteriousDoorController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using UnityEngine;
+
+namespace LuminaTerra;
+
+public class MysteriousDoorController : MonoBehaviour
+{
+    [SerializeField] private InteractReceiver _interactReceiver;
+    [SerializeField] private OWAudioSource _loopingAudio;
+
+    private Animator _animator;
+
+    private void Awake()
+    {
+        _animator = GetComponent<Animator>();
+        _interactReceiver.OnPressInteract += OnPressInteract;
+    }
+
+    private void Start()
+    {
+        _interactReceiver.ChangePrompt("Open Door");
+    }
+
+    private void OnPressInteract()
+    {
+        _animator.SetTrigger("Open");
+        _interactReceiver.DisableInteraction();
+    }
+
+    public void OnDoorOpen()
+    {
+        _loopingAudio.FadeIn(2f, true, true);
+    }
+
+    private void OnDestroy()
+    {
+        _interactReceiver.OnPressInteract -= OnPressInteract;
+    }
+}
